1c2e388b61102d2ce6f769763f4052d28ddd60d1
Manu Zarraga <manuzarraga@hotmail.com>
2024-02-08 19:45:18 -0300

    feat: adding project structure and TopLeft component

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,14 @@
+"use-client";
+// MUI Imports
+import Box from "@mui/material/Box";
+
+// Components
+import TopLeft from "@/components/TopLeft/TopLeft";
+
+export default function Home() {
+  return (
+    <Box sx={{ p: ".5rem" }}>
+      <TopLeft />
+    </Box>
+  );
+}
---
+++ src/components/TopLeft/TopLeft.tsx
@@ -0,0 +1,53 @@
+"use-client";
+// MUI Imports
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+
+// Phosphor
+import { House } from "@phosphor-icons/react/dist/ssr";
+import { SignIn } from "@phosphor-icons/react/dist/ssr";
+
+// ? Add onClick functions for navigation to Home and LogIn pages.
+
+export default function TopLeft() {
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        gap: "1rem",
+        flexDirection: "column",
+        backgroundColor: "#212121",
+        px: "2rem",
+        py: "2rem",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "1rem",
+          ":hover": { cursor: "pointer", bgcolor: "red" },
+        }}
+      >
+        <House size={30} color="#B3B3B3" weight="regular" />
+        <Typography variant="subtitle1" color="#B3B3B3" fontWeight="bold">
+          Home
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: "1rem",
+          ":hover": { cursor: "pointer", bgcolor: "red" },
+        }}
+      >
+        <SignIn size={30} color="#B3B3B3" weight="regular" />
+        <Typography variant="subtitle1" color="#B3B3B3" fontWeight="bold">
+          Sign In
+        </Typography>
+      </Box>
+    </Card>
+  );
+}
